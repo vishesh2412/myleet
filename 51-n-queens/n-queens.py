85ff5de8b7a8741ba105847ea8fe1e7c768d0c96
Vishesh Goyal <98270929+vishesh2412@users.noreply.github.com>
2025-06-09 19:32:48 +0530

    Time: 13 ms (68.38%) | Memory: 12.6 MB (73.94%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,37 +1,31 @@
 class Solution(object):
     def solveNQueens(self, n):
-        def isSafe(board,row,col,n):
-            for i in range(n):    
-                if board[i][col]=='Q':
-                    return False
-            i=row
-            j=col
-            while(i>=0 and j>=0):
-                if board[i][j]=='Q':
-                    return False
-                i=i-1
-                j=j-1
-            i=row
-            j=col
-            while(i>=0 and j<n):
-                if board[i][j]=='Q':
-                    return False
-                i=i-1
-                j=j+1
-            return True
-
-        def nQueens(board,row,n):
+        def backtrack(board,row):
             if row==n:
-                ans.append([''.join(k) for k in board])
+                res.append([''.join(r) for r in board])
                 return
-    
-            for j in range(n):
-                if isSafe(board,row,j,n):
-                    board[row][j]='Q'
-                    nQueens(board,row+1,n)
-                    board[row][j]='.'
 
-        ans=[]
-        board=[["." for _ in range(n)] for _ in range(n)]
-        board=nQueens(board,0,n)
-        return ans
+            for i in range(n):
+                if i in col or row-i in d1 or row+i in d2:
+                    continue
+                
+                col.add(i)
+                d1.add(row-i)
+                d2.add(row+i)
+                board[row][i]='Q'
+
+                backtrack(board,row+1)
+
+                col.remove(i)
+                d1.remove(row-i)
+                d2.remove(row+i)
+                board[row][i]='.'
+
+
+        res=[]
+        board=[['.']*n for _ in range(n)]
+        col=set()
+        d1=set()
+        d2=set()
+        backtrack(board,0)
+        return res
