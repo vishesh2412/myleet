8a80b161d40b3b8c262dbd0a7f33a002d7c49c91
Vishesh Goyal <98270929+vishesh2412@users.noreply.github.com>
2025-06-09 19:31:56 +0530

    Time: 9 ms (79.45%) | Memory: 12.6 MB (15.37%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,40 +1,31 @@
 class Solution(object):
     def totalNQueens(self, n):
-        def isSafe(board,row,col,n):
-            for i in range(n):
-                if board[i][col]=='Q':
-                    return False
-                
-            i=row
-            j=col
-            while(i>=0 and j>=0):
-                if board[i][j]=='Q':
-                    return False
-                i-=1
-                j-=1
-
-            i=row
-            j=col
-            while(i>=0 and j<n):
-                if board[i][j]=='Q':
-                    return False
-                i-=1
-                j+=1
-            
-            return True
-
-        def nQueens(board,row,n):
-            if(row==n):
-                ans.append([''.join(k) for k in board])
+        def backtrack(board,row):
+            if row==n:
+                res.append([''.join(r) for r in board])
                 return
-            
+
             for i in range(n):
-                if isSafe(board,row,i,n):
-                    board[row][i]='Q'
-                    nQueens(board,row+1,n)
-                    board[row][i]='.'
-        
-        ans=[]
-        board=[['.' for _ in range(n)]for _ in range(n)]
-        board=nQueens(board,0,n)
-        return len(ans)
+                if i in col or row-i in d1 or row+i in d2:
+                    continue
+                
+                col.add(i)
+                d1.add(row-i)
+                d2.add(row+i)
+                board[row][i]='Q'
+
+                backtrack(board,row+1)
+
+                col.remove(i)
+                d1.remove(row-i)
+                d2.remove(row+i)
+                board[row][i]='.'
+
+
+        res=[]
+        board=[['.']*n for _ in range(n)]
+        col=set()
+        d1=set()
+        d2=set()
+        backtrack(board,0)
+        return len(res)
