be2491da7c5729f08b6443db5475a560d134c85e
Vishesh Goyal <98270929+vishesh2412@users.noreply.github.com>
2025-07-09 01:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.00%) - LeetSync

--- 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.py
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.py
@@ -1,14 +1,13 @@
 class Solution(object):
     def numMovesStones(self, a, b, c):
-        answer=[]
         new=sorted([a,b,c])
         if abs(new[0]-new[1])==1 and abs(new[1]-new[2])==1:
-            answer.append(0)
+            answer1=0
         elif abs(new[0]-new[1])<=2 or abs(new[1]-new[2])<=2:
-            answer.append(1)
+            answer1=1
         else:
-            answer.append(2)
+            answer1=2
         
-        answer.append(abs(new[0]-new[1])+abs(new[1]-new[2])-2)
+        answer2=abs(new[0]-new[1])+abs(new[1]-new[2])-2
 
-        return answer
+        return [answer1,answer2]
