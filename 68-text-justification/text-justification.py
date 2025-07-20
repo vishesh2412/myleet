fcd8d8a60d053d566c123ac32ef8d44dd5fea0e3
Vishesh Goyal <98270929+vishesh2412@users.noreply.github.com>
2025-07-21 04:09:24 +0530

    Time: 1 ms (17.05%) | Memory: 12.5 MB (15.27%) - LeetSync

--- 68-text-justification/text-justification.py
+++ 68-text-justification/text-justification.py
@@ -1,29 +1,35 @@
 class Solution(object):
     def fullJustify(self, words, maxWidth):
-        reach=0
-        string=[]
-        answer=[]
-        for i in words:
-            if reach+len(i)+len(string)>maxWidth:
-                extra=maxWidth-reach
-                count=len(string)-1
-                if count==0:
-                    adding=string[0]+' '*extra
-                else:
-                    spaces=extra//count
-                    left=extra%count
-                    new_line=''
-                    for j,word in enumerate(string[:-1]):
-                        space_now=spaces+(1 if j<left else 0)
-                        new_line+=(word+' '*space_now)
-                    new_line+=string[-1]
-                    adding=new_line
-                answer.append(adding)
-                reach=len(i)
-                string=[i]
+        res = []
+        i = 0
+        while i < len(words):
+            # Determine how many words fit into the current line
+            line_len = len(words[i])
+            j = i + 1
+            while j < len(words) and line_len + len(words[j]) + (j - i) <= maxWidth:
+                line_len += len(words[j])
+                j += 1
+
+            # Number of words in this line
+            num_words = j - i
+            # Number of spaces to distribute
+            space_needed = maxWidth - line_len
+
+            line = ""
+            if j == len(words) or num_words == 1:
+                # Last line or a line with one word - left justified
+                line = ' '.join(words[i:j])
+                line += ' ' * (maxWidth - len(line))
             else:
-                string.append(i)
-                reach+=len(i)
-        last=' '.join(string)
-        answer.append(last+' '*(maxWidth-len(last)))
-        return answer
+                # Middle justified line
+                spaces = space_needed // (num_words - 1)
+                extra = space_needed % (num_words - 1)
+
+                for k in range(i, j - 1):
+                    line += words[k]
+                    line += ' ' * (spaces + (1 if k - i < extra else 0))
+                line += words[j - 1]  # last word in the line
+
+            res.append(line)
+            i = j
+        return res
