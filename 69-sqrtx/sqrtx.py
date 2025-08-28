4678bca52487a92ca9d10cadcc7cea59498279a0
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-08-29 01:50:34 +0530

    Time: 2152 ms (5.01%) | Memory: 17.7 MB (81.36%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,3 +1,11 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        if x==1 or x==2 or x==3:
+            return 1
+        if x==5:
+            return 2
+        for i in range(int(x/2)+1):
+            if i**2>x:
+                return i-1
+            elif i**2==x:
+                return i
