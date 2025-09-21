7a1c163b82baa7ccc9dae25b435ffe13c6315f10
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-09-21 11:28:57 +0530

    Time: 10 ms (93.37%) | Memory: 19.8 MB (87.83%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -3,14 +3,18 @@ from typing import List
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        
-        # dp[j] = min path sum from current row (i) to bottom-right, starting at column j
-        dp = [float('inf')] * (n + 1)
-        dp[n - 1] = 0  # base case, just right of bottom-right cell
 
-        # fill from bottom → top, right → left
-        for i in reversed(range(m)):
-            for j in reversed(range(n)):
-                dp[j] = grid[i][j] + min(dp[j], dp[j + 1])
+        # fill last row
+        for j in range(n - 2, -1, -1):
+            grid[m - 1][j] += grid[m - 1][j + 1]
 
-        return dp[0]
+        # fill last column
+        for i in range(m - 2, -1, -1):
+            grid[i][n - 1] += grid[i + 1][n - 1]
+
+        # fill the rest
+        for i in range(m - 2, -1, -1):
+            for j in range(n - 2, -1, -1):
+                grid[i][j] += min(grid[i + 1][j], grid[i][j + 1])
+
+        return grid[0][0]
