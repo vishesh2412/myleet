ad6e481524ba000039e39420a1a68140eddb9a1c
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-09-24 12:50:05 +0530

    Time: 261 ms (87.79%) | Memory: 44.7 MB (48.44%) - LeetSync

--- 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.py
+++ 3823-count-islands-with-total-value-divisible-by-k/count-islands-with-total-value-divisible-by-k.py
@@ -3,10 +3,10 @@ class Solution:
         def traverse(i,j):
             if i<0 or j<0 or i>=n or j>=m or grid[i][j]==0:
                 return 0
-            a=grid[i][j]
+            val=grid[i][j]
             grid[i][j]=0
 
-            return a+traverse(i+1,j)+traverse(i,j+1)+traverse(i-1,j)+traverse(i,j-1)
+            return val+traverse(i+1,j)+traverse(i,j+1)+traverse(i-1,j)+traverse(i,j-1)
 
 
         n=len(grid)
