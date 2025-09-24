486e0e4572ecf83d08d7a2b02e11056941d32c61
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-09-24 12:44:44 +0530

    Time: 11 ms (97.69%) | Memory: 18.2 MB (79.78%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def traverse(i,j):
-            if i<0 or j<0 or i>=rows or j>=cols or grid[i][j]==0:
+            if i<0 or j<0 or i>=n or j>=m or grid[i][j]==0:
                 return 0
             grid[i][j]=0
 
             return 1+traverse(i+1,j)+traverse(i,j+1)+traverse(i-1,j)+traverse(i,j-1)
 
 
-        rows=len(grid)
-        cols=len(grid[0])
+        n=len(grid)
+        m=len(grid[0])
         area=0
 
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]==1:
                     area=max(area,traverse(i,j))
         
