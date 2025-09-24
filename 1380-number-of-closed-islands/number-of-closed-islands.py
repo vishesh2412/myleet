fce0ac99cb8c213c50ec0daa3afdfbaae6850707
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-09-24 13:20:04 +0530

    Time: 15 ms (75.73%) | Memory: 18.7 MB (33.81%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.py
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -1,26 +1,27 @@
 class Solution:
-    def closedIsland(self, grid: list[list[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= rows or c >= cols:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        def traverse(i,j):
+            if i<0 or j<0 or i>=rows or j>=cols:
                 return False
-            if grid[r][c] == 1:
+            if grid[i][j]==1:
                 return True
-            
-            grid[r][c] = 1  # mark as visited
-            
-            top = dfs(r-1, c)
-            bottom = dfs(r+1, c)
-            left = dfs(r, c-1)
-            right = dfs(r, c+1)
-            
+            grid[i][j]=1
+
+            top=traverse(i,j-1)
+            bottom=traverse(i,j+1)
+            left=traverse(i-1,j)
+            right=traverse(i+1,j)
+
             return top and bottom and left and right
-        
-        count = 0
+
+        rows=len(grid)
+        cols=len(grid[0])
+        count=0
+
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 0:
-                    if dfs(i, j):
-                        count += 1
+                if grid[i][j]==0:
+                    if traverse(i,j):
+                        count+=1
+        
         return count
