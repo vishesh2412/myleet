7c34b49738ed4b5c0668c3c038db815a34b3c47d
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-10-18 21:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.23%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -8,9 +8,11 @@ class Solution(object):
     def inorderTraversal(self, root):
         result=[]
         def traverse(node):
-            if node:
-                traverse(node.left)
-                result.append(node.val)
-                traverse(node.right)
+            if not node:
+                return
+            traverse(node.left)
+            result.append(node.val)
+            traverse(node.right)
+        
         traverse(root)
         return result
