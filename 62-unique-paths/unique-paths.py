feeefd7b84fdeb2e7b9e93a8d6fd2717cd488505
Vishesh <98270929+vishesh2412@users.noreply.github.com>
2025-10-29 21:13:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.46%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,4 +1,10 @@
-import math
+from functools import lru_cache
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return (math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1)))
+        @lru_cache(None)
+        def dp(m,n):
+            if m==1 or n==1:
+                return 1
+            else:
+                return dp(m-1,n)+dp(m,n-1)
+        return dp(m,n)
